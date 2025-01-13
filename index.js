0404fc89322f8f065e405ef703a222eccf5bd84c
longthanhdam <damthanhlong@gmail.com>
2025-01-13 07:11:33 +0000

    Add content index.js

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const PORT = 3000;
 app.get('/', (req, res) => {
     res.send('Hello from Node.js with Nginx and Kubernetes!');
 });
+// Route thứ hai
+app.get('/update', (req, res) => {
+    res.send('Update Node.js with Nginx and Kubernetes! 1st');
+});
 
 app.listen(PORT, () => {
     console.log(`App running on http://localhost:${PORT}`);
